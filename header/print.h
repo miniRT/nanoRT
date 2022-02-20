690f35c26bb6ba894601b8d2c68e6eb00c5972b3
taeskim <57426861+taeskim-42@users.noreply.github.com>
2022-02-20 19:32:16 +0900

    minor indent

--- header/print.h
+++ header/print.h
@@ -4,6 +4,6 @@
 # include <stdio.h>
 # include "structures.h"
 
-void    write_color(t_color3 pixel_color);
+void	write_color(t_color3 pixel_color);
 
 #endif
--- header/scene.h
+++ header/scene.h
@@ -4,10 +4,9 @@
 # include "structures.h"
 # include "utils.h"
 
-t_canvas    canvas(int  width, int height);
-t_camera    camera(t_canvas *canvas, t_point3 origin);
-
-t_object    *object(t_object_type type, void *element);
-t_sphere    *sphere(t_point3 center, double radius);
+t_canvas	canvas(int width, int height);
+t_camera	camera(t_canvas *canvas, t_point3 origin);
+t_object	*object(t_object_type type, void *element);
+t_sphere	*sphere(t_point3 center, double radius);
 
 #endif
