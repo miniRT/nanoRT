558c778e0cdec0398020ed860c3cac7020b1ba7b
GulSam00 <gulsamcono@gmail.com>
2022-02-28 16:33:08 +0900

    Day 8 : Diffuse(난반사) 구현. 예제와 다른 코드.

--- src/trace/ray/phong_lighting.c
+++ src/trace/ray/phong_lighting.c
@@ -1,5 +1,20 @@
 #include "trace.h"
 
+t_color3    point_light_get(t_scene *scene, t_light *light)
+{
+    t_color3    diffuse; 
+    t_vec3      light_dir; // 빛의 방향 벡터
+    double      kd; // 난반사의 강도
+    
+    light_dir = vunit(vminus(light->origin, scene->rec.p)); //교점에서 출발하여 광원을 향하는 벡터(정규화)
+    // cosΘ는 Θ 값이 90도 일 때 0이고 Θ가 둔각이 되면 음수가 되므로 0.0보다 작은 경우는 0.0으로 대체한다.
+    kd = fmax(vdot(scene->rec.normal, light_dir), 0.0);// (교점에서 출발하여 광원을 향하는 벡터)와 (교점에서의 법선벡터)의 내적값.
+    // fmax 함수는 두 개의 인자 중 큰 값을 리턴한다. 만약 코사인세타가 둔각이 될 경우 음수가 되기에 0을 리턴하도록 한다.
+    diffuse = vmult(light->light_color, kd);
+    return (diffuse);
+}   
+
+
 t_color3    phong_lighting(t_scene *scene)
 {
     t_color3    light_color;
@@ -7,12 +22,12 @@ t_color3    phong_lighting(t_scene *scene)
 
     light_color = color3(0, 0, 0);
     lights = scene->light;
-    // while (lights) // 존재하는 모든 광원들에 대한 정반사, 난반사 값을 연결리스트로 돌아가면서 구해준다.
-    // {
-    //     if (lights->type == LIGHT_POINT)
-    //         light_color = vplus(light_color, point_light_get(scene, lights->element));
-    //         lights = lights->next;
-    // }
+    while (lights) // 존재하는 모든 광원들에 대한 정반사, 난반사 값을 연결리스트로 돌아가면서 구해준다.
+    {
+        if (lights->type == LIGHT_POINT)
+            light_color = vplus(light_color, point_light_get(scene, lights->element));
+            lights = lights->next;
+    }
     // 정반사, 난반사 값을 더했다면 주변광을 더해준다.
     light_color = vplus(light_color, scene->ambient);
         return (vmin(vmult_(light_color, scene->rec.albedo), color3(1, 1, 1)));
