fd0b220739698a1cc63fa7388bdff8159d53ee40
GulSam00 <gulsamcono@gmail.com>
2022-03-15 16:38:56 +0900

    Feat  : 빈 공간 출력.

--- includes/trace.h
+++ includes/trace.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   trace.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kimtaeseon <kimtaeseon@student.42.fr>      +#+  +:+       +#+        */
+/*   By: sham <sham@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:52:09 by kimtaeseon        #+#    #+#             */
-/*   Updated: 2022/03/01 17:28:28 by kimtaeseon       ###   ########.fr       */
+/*   Updated: 2022/03/15 16:25:10 by sham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 t_ray		ray(t_point3 origin, t_vec3 dir);
 t_point3	ray_at(t_ray *ray, double t);
-t_ray		ray_primary(t_camera *cam, double u, double v);
+t_ray		ray_primary(t_camera *cam, int x, int y);
 t_color3	ray_color(t_ray *r);
 
 #endif
